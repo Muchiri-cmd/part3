d61f2552a50600b853c59688134e8e3d634536a1
Muchiri-cmd <davismuchiri21@gmail.com>
2024-07-17 14:51:07 +0300

    Install middleware and include HTTP requests
        - Install Node's cors middleware to allow CORS
        - Include .rest HTTP requests for test purposes

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const express = require('express')
-const morgan = require('morgan')
-
 const app = express()
-app.use(express.json())
 
+const morgan = require('morgan')
 app.use(morgan('tiny'))
+
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.json())//json-parser
 
 morgan.token('postData',function(req,res){
     return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 
-
-
 let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
-      "number": "040-123456"
+      "phoneNumber": "040-123456"
     },
     { 
       "id": "2",
       "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      "phoneNumber": "39-44-5323523"
     },
     { 
       "id": "3",
       "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      "phoneNumber": "12-43-234345"
     },
     { 
       "id": "4",
       "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      "phoneNumber": "39-23-6423122"
     }
 ]
 
@@ -72,15 +73,15 @@ app.post('/api/persons',(req,res)=>{
     const body = req.body
     if (!body.name){
         return res.status(400).json({error:'Name is missing'})
-    } else if (!body.number){
-        return res.status(400).json({error:'Number is missing'})
+    } else if (!body.phoneNumber){
+        return res.status(400).json({error:'phoneNumber is missing'})
     } else if (persons.find(person=> person.name == body.name)){
         return res.status(400).json({error:'name must be unique'})
     }
     const person = {
         id:generateId(),
         name:body.name,
-        number:body.number
+        phoneNumber:body.phoneNumber
     }
     persons = persons.concat(person)
     // console.log(person);
@@ -95,7 +96,7 @@ app.delete('/api/persons/:id',(req,res)=>{
 })
 
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}`);
 })
