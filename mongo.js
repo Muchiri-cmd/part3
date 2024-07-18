d277fe0fdbb9b2d873fab8d1d0342c6cdd2118f6
Muchiri-cmd <davismuchiri21@gmail.com>
2024-07-18 13:26:28 +0300

    Fix: Connection Timeout errors
        - Include serverSelectionTimeoutMS, socketTimeoutMS & family options
         to fine-tune the connection.
        - Refactor to make code more clean & output neater

--- mongo.js
+++ mongo.js
@@ -9,23 +9,21 @@ const [password, new_name, new_phoneNumber] = [process.argv[2], process.argv[3],
 const url = `mongodb+srv://davismuchiri21:${password}@cluster1.om7qdwb.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster1`
 
 mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    phoneNumber: String
+mongoose.connect(url,{
+    serverSelectionTimeoutMS: 5000, 
+    socketTimeoutMS: 45000,        
+    family: 4 
 })
 
+const personSchema = new mongoose.Schema({name: String,phoneNumber: String})
 const Person = mongoose.model('Person',personSchema)
 const record = new Person({name:new_name,phoneNumber:new_phoneNumber})
 
-record.save().then(console.log(`added ${new_name} number ${new_phoneNumber} to phonebook`))
+record.save().then(console.log(`added ${new_name} number ${new_phoneNumber} to phonebook\n`))
 
 Person.find({})//all people records
     .then(persons=>{
         console.log('phonebook:');
-        persons.forEach(person=>{
-            console.log(`${person.name}-${person.phoneNumber}`)
-        })
+        persons.forEach(person=>console.log(`${person.name} - ${person.phoneNumber}`))
         mongoose.connection.close()
     })
